72ba4d69100d594ffef9987b6d3b7642e36b8601
Pedro <example@example.com>
2024-11-01 09:37:43 -0300

    App com telas de gerenciamento

--- react_native/App.js
+++ react_native/App.js
@@ -1,109 +1,51 @@
-import React, {useState} from 'react';
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+// projeto gerenciamento db petshop
+import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-
-// Montando Janelas
-function Gerenciar() {
-      const DATA = [
-    { id: '1', title: 'Produto 1' },
-    { id: '2', title: 'Produto 2' },
-    { id: '3', title: 'Produto 3' },
-    { id: '4', title: 'Produto 4' },
-    { id: '5', title: 'Produto 5' },
-  ];
-
-    const renderItem = ({ item }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{item.title}</Text>
-    </View>
-  );
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />
-    </View>
-  );
-}
-
-function Pedidos() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Ver pedidos</Text>
-    </View>
-  );
-}
-
+// Importando telas do projeto
+import ProductList from './telas/ListaProdutos';
+import Pedidos from './telas/Pedidos';
 
 const Tab = createBottomTabNavigator();
 
-function MyTabs() {
-  return (
-    <Tab.Navigator
-      initialRouteName="Cadastro"
-      screenOptions={{
-        tabBarActiveTintColor: 'blue',
-      }}
-    >
-      <Tab.Screen
-        name="Gerenciar Produtos"
-        component={Gerenciar}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Pedidos"
-        component={Pedidos}
-        options={{
-          tabBarLabel: 'Pedidos',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="order-bool-descending" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
+// seguindo documentacao do expo
+const MyTabs = () => {
+    return (
+        <Tab.Navigator
+            initialRouteName="GerenciarProdutos"
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+                    let iconName;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-    backgroundColor: '#f8f8f8',
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 1.41,
-    elevation: 2,
-  },
-  title: {
-    fontSize: 18,
-  },
-});
+                    if (route.name === 'GerenciarProdutos') {
+                        iconName = 'home';
+                    } else if (route.name === 'Pedidos') {
+                        iconName = 'order-bool-descending';
+                    }
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
-  );
-}
+                    return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: 'blue', // indigo roxo
+                tabBarInactiveTintColor: 'gray',
+            })}
+        >
+            <Tab.Screen name="GerenciarProdutos" component={ProductList} options={{tabBarLabel:"Home"}}/>
+            <Tab.Screen name="Pedidos" component={Pedidos} options={{tabBarLabel:"Pedidos"}}/>
+        </Tab.Navigator>
+    );
+};
+
+const App = () => {
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <NavigationContainer>
+                <MyTabs />
+            </NavigationContainer>
+        </GestureHandlerRootView>
+    );
+};
+
+export default App;
